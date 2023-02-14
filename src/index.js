facd25fa8a46bc77c3b4001dc7c70a2789326a43
shishishi76 <ssshogo4405@gmail.com>
2023-02-14 07:40:31 +0000

    アロー関数

--- src/index.js
+++ src/index.js
@@ -8,3 +8,42 @@
 //   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 // </div>
 // `;
+
+/**
+ * アロー関数
+ */
+//従来の関数
+// function func1(str) {
+//     return str;
+// }
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("func1です"));
+
+// //アロー関数
+// const func2 = (str) => {
+//   return str;
+// };
+// console.log("func2です");
+
+// //アロー関数の中身が一行の時のみ省略版
+// const func2_1 = (str) => str;
+// console.log(func2_1("func2_1です"));
+
+// //引数二つバージョン
+// const func3 = (num1, num2) => {
+//   return num1 + num2;
+// };
+// console.log(func3(5, 123));
+
+/**
+ * 分割代入
+ */
+
+const myProfile = {
+  name: "しょうご",
+  age: 23,
+};
+
+const message1 = `名前は${name}です。年齢は${age}です。`;
