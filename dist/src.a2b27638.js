5ec06c5579cd411da06b150586f1f9afeef06786
shishishi76 <ssshogo4405@gmail.com>
2023-02-14 07:52:47 +0000

    Update 3 files

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -174,13 +174,14 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 // const message2 = `名前は${name}です。年齢は${age}です。`;
 // console.log(message1);
 
-var myProfile = ["しょうご", 23];
-var message3 = "\u540D\u524D\u306F".concat(myProfile[0], "\u3067\u3059\u3002\u5E74\u9F62\u306F").concat(myProfile[1], "\u3067\u3059\u3002");
-console.log(message3);
-var name = myProfile[0],
-  age = myProfile[1];
-var message4 = "\u540D\u524D\u306F".concat(name, "\u3067\u3059\u3002\u5E74\u9F62\u306F").concat(age, "\u6B73\u3067\u3059\u3002");
-console.log(message4);
+// const myProfile = ["しょうご", 23];
+
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}です。`;
+// console.log(message3);
+
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message4);
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
--- src/index.js
+++ src/index.js
@@ -54,11 +54,11 @@
 // const message2 = `名前は${name}です。年齢は${age}です。`;
 // console.log(message1);
 
-const myProfile = ["しょうご", 23];
+// const myProfile = ["しょうご", 23];
 
-const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}です。`;
-console.log(message3);
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}です。`;
+// console.log(message3);
 
-const [name, age] = myProfile;
-const message4 = `名前は${name}です。年齢は${age}歳です。`;
-console.log(message4);
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message4);
